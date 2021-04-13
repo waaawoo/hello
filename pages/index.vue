0d2811726b7affd621e5b2ae6f46107aba91e569
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-13 11:40:24 +0900

    画面遷移

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
     <!-- Nuxt.jsはほっとりローディングする（自動で編集を反映してくれる）Dev環境のみ -->
     <p>Hello World</p>
     <p>{{message}}</p>
+    <hr>
+    <!-- 画面遷移 -->
+    <router-link to="/price">pricePage</router-link>
   </section>
 </template>
 
---
+++ pages/price.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section>
+      <h1>Price Page</h1>
+      <hr>
+      <!-- 画面遷移 -->
+      <router-link to="/">TopPage</router-link>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
